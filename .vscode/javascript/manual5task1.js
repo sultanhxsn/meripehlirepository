0bc0eb78ebc35a9ec2fdaa9418ffc78c4de2a21b
sultanhxsn <sultanhasanminai@gmail.com>
2024-11-28 23:08:50 +0500

    Initial commit

---
+++ .vscode/javascript/manual5task1.js
@@ -0,0 +1,4 @@
+const checkEligibility = (name, age) => 
+{
+  return age >= 18 ? `${name}, you are eligible to vote.` : `${name}, you are not eligible to vote.`;
+};
---
+++ .vscode/script.js
@@ -0,0 +1,8 @@
+let length=10;
+let width=5;
+let product=length*width;
+
+document.getElementById("final").innerText = `the area of the rectangle is ${product}`;
+
+
+
